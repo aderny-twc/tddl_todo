67ca242a30313d7b079f48f1aecb0131673f1d6c
Andrey Belskiy <diksandab@gmail.com>
2021-03-16 10:49:06 +0600

    Updated tests with unittest

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -1,7 +1,28 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http:/localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    """Тест нового посетителя."""
 
-assert 'Django' in browser.title
+    def setUp(self):
+        """Установка"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Закрытие браузера"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Можно начать список и получить его позже"""
+        #Получим домашнюю страницу
+        self.browser.get('http:/localhost:8000')
+        
+        #Проверка заголовка
+        self.assertIn('To-Do', self.browser.title)
+        #Генерация сообщения ошибки
+        self.fail('End of test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
