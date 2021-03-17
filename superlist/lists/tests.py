cf56b1e20d7785f1a352046d04df7979abc77609
Andrey Belskiy <diksandab@gmail.com>
2021-03-17 10:14:54 +0600

    Базовое представление возвращает html. Добавлено тестирование домашней страницы

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
+#from views import home_page
 from lists.views import home_page
 
 
@@ -11,3 +14,15 @@ class HomePageTest(TestCase):
         # Преобразование адреса и нахождение функции представления
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        """Домашняя страница возвращает правильный html"""
+        # Объект при запросе страницы
+        request = HttpRequest()
+        # Передача представлению
+        response = home_page(request)
+        # Извлечение содержимого
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- superlist/lists/views.py
+++ superlist/lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def home_page():
+def home_page(request):
     """Home page"""
-    pass
+    return HttpResponse('<html><title>To-Do lists</title></html>')
