a6861bb0e75a06d4a434684cd978859f0636b4c3
Andrey Belskiy <diksandab@gmail.com>
2021-03-17 10:52:47 +0600

    Функциональный тест позволяет проверить ввод нового элемента списка

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -20,8 +22,29 @@ class NewVisitorTest(unittest.TestCase):
         
         #Проверка заголовка
         self.assertIn('To-Do', self.browser.title)
-        #Генерация сообщения ошибки
-        self.fail('End of test!')
+        # Получим элемент заголовка
+        header_test = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                            'Enter a to-do item')
+
+        # Ввод нового пункта
+        inputbox.send_keys('By new item')
+
+        # Нажатие enter, обновление страницы
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: By new item' for row in rows)
+                )
+        # Созадание еще одного элемента ...
+        self.fail('Закончить тест')
+
 
 
 if __name__ == '__main__':
