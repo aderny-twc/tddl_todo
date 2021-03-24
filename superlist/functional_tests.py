c3e76a7a20438df493226d8a3a5b5797ca7e677b
Andrey Belskiy <diksandab@gmail.com>
2021-03-24 22:14:57 +0600

    Added new functionality. Now user can add new item in the todo list

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -19,31 +19,36 @@ class NewVisitorTest(unittest.TestCase):
         """Можно начать список и получить его позже"""
         #Получим домашнюю страницу
         self.browser.get('http:/localhost:8000')
-        
+
         #Проверка заголовка
         self.assertIn('To-Do', self.browser.title)
         # Получим элемент заголовка
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
                             'Enter a to-do item')
 
         # Ввод нового пункта
-        inputbox.send_keys('By new item')
+        inputbox.send_keys('Buy a new item')
 
         # Нажатие enter, обновление страницы
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        #Добавление еще одного элемента
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('This is the second element')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: By new item' for row in rows),
-                "Новый элемент списка не появился в таблице",
-                )
+        self.assertIn('1: Buy a new item', [row.text for row in rows])
+        self.assertIn('2: This is the second element', [row.text for row in rows])
+
         # Созадание еще одного элемента ...
+        # Сайт запоминает текущий список пользователя
         self.fail('Закончить тест')
 
 
--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -19,10 +19,15 @@ class HomePageTest(TestCase):
     def test_test_server_django_returns_correct_html(self):
         """Home page returns right html"""
         response = self.client.get('/')
-
         #html = response.content.decode('utf8')
         #self.assertTrue(html.startswith('<html>'))
         #self.assertIn('<title>To-Do lists</title>', html)
         #self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'lists/home.html')
 
+    def test_can_save_a_POST_request(self):
+        """Test: saving POST-request."""
+        response = self.client.post('/',
+                                    data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'lists/home.html')
