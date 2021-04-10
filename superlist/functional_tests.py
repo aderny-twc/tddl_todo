ccfd10615b68eeec69558796ea94e246290a1114
Andrey Belskiy <diksandab@gmail.com>
2021-04-10 15:22:55 +0600

    Added new tests

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
         """Закрытие браузера"""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Checking the new string in the table"""
+        talbe = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Можно начать список и получить его позже"""
         #Получим домашнюю страницу
@@ -30,12 +36,11 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                             'Enter a to-do item')
 
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # Ввод нового пункта
         inputbox.send_keys('Buy a new item')
 
-        # Нажатие enter, обновление страницы
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         #Добавление еще одного элемента
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('This is the second element')
