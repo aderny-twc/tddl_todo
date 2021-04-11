a17cb693d72903e6dd0bc7cb7dafffd67e1a5693
Andrey Belskiy <diksandab@gmail.com>
2021-04-11 15:39:19 +0600

    Added new test, updated views

--- superlist/lists/views.py
+++ superlist/lists/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from lists.models import Item
 
 
 def home_page(request):
@@ -7,6 +9,11 @@ def home_page(request):
     #if request.method == 'POST':
     #    return HttpResponse(request.POST['item_text'])
     #return render(request, 'lists/home.html')
-    return render(request, 'lists/home.html', {
-            'new_item_text': request.POST.get('item_text', ''),
-        })
+    #item = Item()
+    #item.text = request.POST.get('item_text', '')
+    #item.save()
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    return render(request, 'lists/home.html')
